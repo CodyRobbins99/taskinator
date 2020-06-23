6c771d140165decbaa1af494ec22d0a6b247461c
Coodie <65614373+CodyRobbins99@users.noreply.github.com>
2020-06-23 18:03:21 -0400

    code refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,26 @@
 var formEl = document.querySelector("#task-form")
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// Function To Create Task Item
-var createTaskHandler = function(event) {
+// Task Form Handler Function
+var taskFormHandler = function(event) {
 
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // package data as an object 
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+// Function to Create Task Element
+var createTaskEl = function(taskDataObj) {
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -19,13 +31,13 @@ var createTaskHandler = function(event) {
     //give it a class name
     taskInfoEl.className = "task-info";
     // add GTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
     listItemEl.appendChild(taskInfoEl)
-   
+
     // ad entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
 // Button Click Listener
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
